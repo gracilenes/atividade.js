084843f17c5763f0cc8cbaea65a0de4b5dbb3d6f
gracilenes <gracilenes876@gmail.com>
2024-10-14 17:21:58 +0000

    giuyhinhj

---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+//alert("Hello World");
+
+// for(inicialização; condição; atualização)
+for (i = 0;i < 1000;i++) {
+    console.log(i);
+    
+    if(i >= 10) {
+        break
+    }
+}
+
+/*
+// == apenas o valor 5 == "5"
+// === valor e tipo 5 === 5
+
+var notaInt = parseInt(nota);
+
+switch(nota) {
+    case 0:
+    case 1:
+    case 2:
+    case 3: 
+    case 4:
+    case 5:
+    case 6:    
+        alert("Reprovado!");
+        break;
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+            alert("Aprovado!");
+            break;
+            default:
+                alert("Nota inválida.");
+                break
+}
+
+if(notaInt < 40) {
+    //reprovado
+} else if(nota < 70) {
+    //recuperação
+} else if(nota <= 70) {
+    //aprovado
+} else {
+    //inválida
+}
+*/
+
+
+/*
+var i = 0;
+
+while(i < 3) {
+    console.log("valor de i:")
+    var nota = prompt("Digite sua nota:");
+    i++; // i = i + 1; i += 1;
+}
+*/
+
